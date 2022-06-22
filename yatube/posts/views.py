a075506a28673817813129bbbc45f1659c485c51
Рамиль Абзалов <ramadjamal@yandex.ru>
2022-06-22 21:48:22 +0300

    15

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -2,16 +2,27 @@ from django.shortcuts import render, get_object_or_404
 
 from .models import Post, Group
 
+from django.core.paginator import Paginator
+
 LIM_POST: int = 10
 
 
 def index(request):
-    posts = (Post
-             .objects
-             .select_related('group', 'author')
-             .order_by('-pub_date')[:LIM_POST])
+    post_list = Post.objects.all().order_by('-pub_date')
+    # Если порядок сортировки определен в классе Meta модели,
+    # запрос будет выглядить так:
+    # post_list = Post.objects.all()
+    # Показывать по 10 записей на странице.
+    paginator = Paginator(post_list, 10)
+
+    # Из URL извлекаем номер запрошенной страницы - это значение параметра page
+    page_number = request.GET.get('page')
+
+    # Получаем набор записей для страницы с запрошенным номером
+    page_obj = paginator.get_page(page_number)
+    # Отдаем в словаре контекста
     context = {
-        'posts': posts,
+        'page_obj': page_obj,
     }
     return render(request, 'posts/index.html', context)
 
